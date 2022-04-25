e290e5b1b0b2e8728acf444c580179a7d9d7fe13
Rupa <Rupa@QQHQNSA>
2022-04-25 21:03:46 +0530

    By Niraj

---
+++ GitHub_April25/src/Demooo.java
@@ -0,0 +1,31 @@
+
+public class Demooo {
+
+	
+	 public void m1()
+	 {
+		 System.out.println("m1 method");
+	 }
+	 
+	 public void m2()
+	 {
+		 System.out.println("m2 method");
+	 }
+	 
+	 public void m3()
+	 {
+		 System.out.println("m3 method");
+	 }
+	 
+	 public static void main(String[] args) 
+		{
+		 Demooo d =new Demooo();
+		 d.m1();
+		 d.m2();
+		 d.m3();
+
+	}
+}
+
+
+
